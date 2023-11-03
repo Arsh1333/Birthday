ad44c65e72672b6b5588f7e4240dd59849a88184
Arsh <pawar.arsh1333@gmail.com>
2023-11-04 01:55:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import BirthdayDisplay from "./components/BirthdayDisplay";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [birthday, setBirthDay] = useState(" ");
+  const btnHandler = () => {
+    console.log("clicked");
+    setBirthDay(<BirthdayDisplay></BirthdayDisplay>);
+  };
   return (
     <div className="App">
       <div className="main-content">
@@ -8,8 +15,11 @@ function App() {
           <input palceholder="Enter Name" type="text"></input>
           <input palceholder="Enter DOB" type="date"></input>
         </div>
-        <button className="btn">Submit</button>
+        <button className="btn" onClick={btnHandler}>
+          Submit
+        </button>
       </div>
+      <div className="birthday-wish">{birthday}</div>
     </div>
   );
 }
---
+++ src/components/BirthdayDisplay.js
@@ -0,0 +1,8 @@
+function BirthdayDisplay() {
+  return (
+    <div className="BirthdayDisplay">
+      <h1>Hi</h1>
+    </div>
+  );
+}
+export default BirthdayDisplay;
