e803b5bc5095c7d205b7e2d8018c803c489120d1
Arsh <pawar.arsh1333@gmail.com>
2023-11-05 13:48:45 +0530

    temporary alert box display

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [birthday, setBirthDay] = useState(" ");
-  const [name, setName] = useState(" ");
-  const btnHandler = (e) => {
-    if (name == " ") {
-      setBirthDay(<BirthdayDisplay></BirthdayDisplay>);
+  const [name, setName] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name === " ") {
+      <BirthdayDisplay></BirthdayDisplay>;
     } else {
-      setName(e.target.value);
+      alert(`Name: ${name}, Birthday: ${birthday}`);
     }
-    console.log("clicked");
   };
   return (
     <div className="App">
       <div className="main-content">
-        <form className="inputs">
+        <form onSubmit={handleSubmit} className="inputs">
           <input
-            palceholder="Enter Name"
             type="text"
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          ></input>
-          <input palceholder="Enter DOB" type="date"></input>
+            placeholder="Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="date"
+            placeholder="Date"
+            value={birthday}
+            onChange={(event) => setBirthday(event.target.value)}
+          />
+          <button type="submit">Submit</button>
         </form>
-        <button className="btn" onClick={btnHandler}>
-          Submit
-        </button>
-      </div>
-      <div className="side-content">
-        <div className="birthday-wish">{birthday}</div>
       </div>
     </div>
   );
