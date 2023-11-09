87678235c50af38b518c1d4b6a4bef850f256401
Arsh <pawar.arsh1333@gmail.com>
2023-11-09 22:15:48 +0530

    Birthdays displayed in main screen on input

--- src/App.js
+++ src/App.js
@@ -4,35 +4,45 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
-  const [birthday, setBirthday] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (name === " ") {
-      <BirthdayDisplay></BirthdayDisplay>;
-    } else {
-      alert(`Name: ${name}, Birthday: ${birthday}`);
-    }
+  const [displayedName, setDisplayedName] = useState("");
+  const [date, setDate] = useState("");
+  const [displayDate, setDisplayDate] = useState("");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+  const handleDisplayButtonClick = (e) => {
+    e.preventDefault();
+    setDisplayedName(name);
+    setDisplayDate(date);
+  };
+
   return (
     <div className="App">
       <div className="main-content">
-        <form onSubmit={handleSubmit} className="inputs">
+        <form className="inputs">
           <input
             type="text"
             placeholder="Name"
             value={name}
-            onChange={(event) => setName(event.target.value)}
+            onChange={handleNameChange}
           />
           <input
             type="date"
             placeholder="Date"
-            value={birthday}
-            onChange={(event) => setBirthday(event.target.value)}
+            value={date}
+            onChange={handleDateChange}
           />
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={handleDisplayButtonClick}>
+            Submit
+          </button>
         </form>
       </div>
+      <p>Birthdays:{displayedName}</p>
+      <p>{displayDate}</p>
+      {/*<p>{name}</p>*/}
     </div>
   );
 }
